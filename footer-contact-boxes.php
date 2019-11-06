e0e0cfa9476232f45a3b8492ea4d10eb5d688a57
jorwsmit <jorwsmith92@gmail.com>
2019-11-06 08:43:15 -0500

    Adding additional.css for overriding classes, modifying php files, and modificiations to style sheet.

--- footer-contact-boxes.php
+++ footer-contact-boxes.php
@@ -15,7 +15,12 @@
                     <div>
                         <?php mesmerize_print_area_social_icons('footer', 'content', 'footer-social-icons', 5);?>
                     </div>
-                    <?php echo mesmerize_get_footer_copyright(); ?>
+                    <?php //echo mesmerize_get_footer_copyright(); ?>
+                    <div>
+						            <img width="473" height="297" src="https://louisvilleinnovation.com/wp-content/uploads/2019/11/LEAP_Logo_RGB.png" class="logo dark" alt="" itemprop="logo" srcset="https://louisvilleinnovation.com/wp-content/uploads/2019/11/LEAP_Logo_RGB.png 473w, https://louisvilleinnovation.com/wp-content/uploads/2019/11/LEAP_Logo_RGB-300x188.png 300w" sizes="(max-width: 473px) 100vw, 473px">
+					          </div>
+                    <p data-footer-copyright="true" class="copyright">&copy;&nbsp;&nbsp;2019&nbsp;LEAP&nbsp;</p>
+                    <p data-footer-copyright="true" class="copyright">Built using WordPress and the <a target="_blank" href="https://extendthemes.com/go/built-with-mesmerize/" class="mesmerize-theme-link customize-unpreviewable">Mesmerize Theme</a></p>
                 </div>
             </div>
         </div>
